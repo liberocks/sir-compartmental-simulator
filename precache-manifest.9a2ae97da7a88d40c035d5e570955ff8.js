e59399a5f9a83c612328f667afaf6cc1703a5eb3
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2020-03-28 14:50:46 +0700

    Updates

---
+++ precache-manifest.9a2ae97da7a88d40c035d5e570955ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d459ae2b4686eb0902e9cbd63e75e0d3",
+    "url": "/sir-compartmental-simulator/index.html"
+  },
+  {
+    "revision": "992ca6e1e185c6af3fa3",
+    "url": "/sir-compartmental-simulator/static/css/main.b7677955.chunk.css"
+  },
+  {
+    "revision": "5811bdf202289aac7b58",
+    "url": "/sir-compartmental-simulator/static/js/2.624016ce.chunk.js"
+  },
+  {
+    "revision": "5811183cee22f306bf32823ffcce5650",
+    "url": "/sir-compartmental-simulator/static/js/2.624016ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992ca6e1e185c6af3fa3",
+    "url": "/sir-compartmental-simulator/static/js/main.219e9346.chunk.js"
+  },
+  {
+    "revision": "532c09069984d5dc4e8a",
+    "url": "/sir-compartmental-simulator/static/js/runtime-main.f29a6e11.js"
+  }
+]);
